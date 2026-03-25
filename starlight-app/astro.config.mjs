ba23ba6710359cbf4a456b4b09e419019286e756
Diogo Silva <8230145@estg.ipp.pt>
2026-03-25 11:36:45 +0000

    correcao

--- starlight-app/astro.config.mjs
+++ starlight-app/astro.config.mjs
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+	// ESTAS DUAS FICAM AQUI (Configuração global do Astro)
+	site: 'https://Diogo1901.github.io',
+	base: '/poc-docs-plataforma', 
+
 	integrations: [
 		starlight({
 			title: 'Minha Plataforma de Docs',
-			base: '/poc-docs-plataforma',
 			sidebar: [
 				{
 					label: 'Documentação',
-					autogenerate: { directory: '' }, // <--- ISTO É A MAGIA!
+					autogenerate: { directory: '' },
 				},
 			],
 		}),
